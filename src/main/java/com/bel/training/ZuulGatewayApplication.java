01baa1275929b6a5424c07142d177a1512932378
vidyadynamicc <vidyabhushan04512@gmail.com>
2022-03-05 14:00:04 +0530

    first commit

---
+++ src/main/java/com/bel/training/ZuulGatewayApplication.java
@@ -0,0 +1,25 @@
+package com.bel.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+	@Bean
+	  public SimpleFilter simpleFilter() {
+	     return new SimpleFilter();
+	  }
+}
